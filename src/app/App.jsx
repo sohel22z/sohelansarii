c0cc7b8cc3aff2e5ccad9c610908210e483c6941
sohel ansari <sohelansarii@outlook.com>
2023-07-02 21:57:02 +0530

    0.7 Add: routes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,43 @@
-function App() {
+import { Link, Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "../pages/home"
+import About from "../pages/about"
+
+const router = createBrowserRouter([
+  {
+    path: "/vite-react-router/",
+    element: <App />,
+    children: [
+      {
+        path: "/vite-react-router/",
+        element: <Home />,
+      },
+      {
+        path: "/vite-react-router/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
 
   return (
+
     <>
-      <h1>Hllo world!</h1>
+
+      <nav>
+        <Link to="/vite-react-router/">Home</Link>
+        {" | "}
+        <Link to="/vite-react-router/contact">Contact</Link>
+      </nav>
+
+      <Outlet />
+
+      <RouterProvider router={router} />
     </>
+
   )
+
 }
 
 export default App;
