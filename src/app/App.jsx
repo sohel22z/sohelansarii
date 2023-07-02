18c780b24cfb6d66d79d9c49f4a964b9e8be33eb
sohel ansari <sohelansarii@outlook.com>
2023-07-02 22:09:37 +0530

    0.8 Add: Config sass

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,16 +1,19 @@
 import Home from "../pages/home"
 import About from "../pages/about"
+import { Link } from "react-router-dom";
 
 const App = () => {
 
   return (
 
     <>
-      <Home />
+      <Link to={<Home />}>Home</Link>
+
       <div>
         <h1>Hello World! </h1>
       </div>
-      <About />
+
+      <Link to={<About />}>About</Link>
     </>
 
   )
