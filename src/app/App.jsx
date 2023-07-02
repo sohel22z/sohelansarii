a0101a2c4fe7afa6b8da0074d8559e849be306d5
sohel ansari <sohelansarii@outlook.com>
2023-07-02 22:06:13 +0530

    0.8

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,37 +1,16 @@
-import { Link, Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "../pages/home"
 import About from "../pages/about"
-
-const router = createBrowserRouter([
-  {
-    path: "/vite-react-router/",
-    element: <App />,
-    children: [
-      {
-        path: "/vite-react-router/",
-        element: <Home />,
-      },
-      {
-        path: "/vite-react-router/about",
-        element: <About />,
-      },
-    ],
-  },
-]);
 
 const App = () => {
 
   return (
 
     <>
-
-      <nav>
-        <Link to="/vite-react-router/">Home</Link>
-        {" | "}
-        <Link to="/vite-react-router/contact">Contact</Link>
-      </nav>
-
-      <RouterProvider router={router} />
+      <Home />
+      <div>
+        <h1>Hello World! </h1>
+      </div>
+      <About />
     </>
 
   )
